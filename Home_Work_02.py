235600514ffd038cb4254c30208abfd7e1051e8e
«Ekaterina <ezhova.kate@mail.ru>
2022-10-12 14:27:28 +0300

    Домашняя работа 2

--- Home_Work_02.py
+++ Home_Work_02.py
@@ -9,7 +9,7 @@ sum = 0
 for i in number:
     if i.isdigit():
         sum += int(i)
-print(f"Сумма чисел {number} равна:", sum) """
+print(f"Сумма чисел в числе '{number}' равна:", sum) """
 
 #Напишите программу, которая принимает на вход число N 
 # и выдает набор произведений чисел от 1 до N.
@@ -19,9 +19,8 @@ print(f"Сумма чисел {number} равна:", sum) """
 """ from unittest import result
 result = 1
 number = int(input('Введите число: '))
-print()
 print('Полученный набор чисел:')
-for i in range(1, number+1):
+for i in range(1, number + 1):
     result *= i
     print(result, end=' ') """
 
@@ -30,9 +29,32 @@ for i in range(1, number+1):
 #Пример:
 #Для n = 6: {1: 2, 2: 2,25, 3: 2,37, 4: 2,44, 5: 2,49, 6: 2,52}
 
-my_dict = {}
+""" my_dict = {}
 my_dict[1] = 2
 number = int(input('Введите число: '))
 for i in range(1, number + 1):
-    my_dict[i] = (1+1/i)**i
-print(f'Последовательность из числа "{number}" = ', my_dict)
+    my_dict[i] = round((1+1/i)**i, 2)
+print(f'Последовательность из числа "{number}" = ', my_dict) """
+
+#Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+#Найдите произведение элементов на введенных пользователем позициях.
+
+""" number = int(input('Введите число: '))
+my_list = []
+for i in range(-number, number + 1):
+    my_list.append(i)
+print('Полученный список:', my_list)
+number_a = int(input('Введите индекс числа из списка для произведения: '))
+number_b = int(input('Введите индекс числа из списка для произведения: '))
+result = my_list[number_a] * my_list[number_b]
+print(f'Произведение числа "{my_list[number_a]}" на число "{my_list[number_b]}" =' ,result) """
+
+# Реализуйте алгоритм перемешивания списка.
+
+import random
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print ("Дан список = ", my_list)
+for i in range(len(my_list)-1, 0, -1):
+    j = random.randint(0, i + 2) 
+    my_list[i], my_list[j] = my_list[j], my_list[i] 
+print ("Список после перемешивания =",  my_list)
