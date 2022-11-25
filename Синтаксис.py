393e4c2ea3ce06b2d6e20356765375a7b2d52020
Екатерина Скоробогатова <110926969+Ekaterina4177@users.noreply.github.com>
2022-11-25 22:05:49 +0300

    Синтаксис 3 лекции

--- Синтаксис.py
+++ Синтаксис.py
@@ -187,6 +187,140 @@
 ## Списки: введение
 ## list = list
 # numbers = [1, 2, 3, 4, 5]
+
+# Урок 3. Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension
+
+# Lambda - анонимный метод функции
+
+""" def f(x):
+    return x**2
+g = f
+
+print(type(f))
+print(type(g))  
+
+def calc(x):
+    return x+10
+#print(calc(10))
+
+def calc2(x):
+    return x*10
+#print(calc(10))
+
+def math(op, x):
+    print(op(x))
+
+math(calc2, 10)
+math(calc, 10)  """
+
+""" def sum(x, y):
+    return x+y """
+
+""" sum = lambda x, y: x+y """
+
+""" def mylt(x, y):
+    return x*y """
+
+""" mylt = lambda x, y: x*y """
+
+""" def calc(op, a, b):
+    print(op(a, b))
+    #return op(a, b)
+    
+calc(lambda x, y: x+y, 4, 5) # Вызов без присваивания функции в переменную """
+
+# List Comprehension
+#[exp for item in iterable]
+# [exp for item in iterable (if conditional)]
+# [exp <if conditional> for item in iterable (if conditional)]
+
+
+""" my_list = []
+ """
+""" for i in range(1, 101):
+    # if (i%2==0):
+    my_list.append(i)
+print(my_list) """
+
+""" def f(x):
+    return x**3
+
+my_list = [(i, f(i)) for i in range(1, 20) if i % 2 == 0]
+print(my_list) """
+
+""" def select(f, col):
+    return [f(x) for x in col]
+
+def where(f, col):
+    return [x for x in col if f(x)]
+
+data = '1 2 3 5 8 15 23 38'.split()
+res = select(int, data)
+res = where(lambda x: not x%2, res)
+res = select(lambda x: (x, x**2), res)
+print(res) """
+
+# map() — это встроенная функция Python,
+# позволяющая обрабатывать и преобразовывать все элементы в итерируемом объекте без цикла for
+
+""" li = [x for x in range(1, 20)]
+li = list(map(lambda x: x+10, li))
+print(li) 
+
+data = list(map(int, input('enter: ').split()))
+
+for e in data:
+    print(e)
+
+print('--')
+for e in data:
+    print(e) """
+
+""" def where(f, col):
+    return [x for x in col if f(x)]
+
+data = '1 2 3 5 8 15 23 38'.split()
+res = list(map(int, data))
+res = where(lambda x: not x%2, res)
+res = list(map(lambda x: (x, x**2), res))
+print(res) """
+
+# Функция filter() применяет указанную функцию к
+# каждому элементу итерируемого объекта и
+# возвращает итератор с теми объектами, для
+# которых функция вернула True.
+
+""" data = [x for x in range(10)]
+
+res = list(filter(lambda x: not x%2, data))
+print(res) """
+
+
+""" data = '1 2 3 5 8 15 23 38'.split()
+res = list(map(int, data))
+res = list(filter(lambda x: not x % 2, res))
+res = list(map(lambda x: (x, x**2), res))
+print(res) """
+
+# Функция zip() применяется к набору итерируемых
+# объектов и возвращает итератор с кортежами из
+# элементов входных данных.
+
+""" users = ['1', '2', '3', '4', '5']
+ids = [4, 5, 9, 14, 7]
+
+data = list(zip(users, ids))
+print(data) """
+
+# Функция enumerate() применяется к итерируемому
+# объекту и возвращает новый итератор с кортежами
+# из индекса и элементов входных данных. (нумерует кортежи с 0)
+
+users = ['1', '2', '3', '4', '5']
+ids = [4, 5, 9, 14, 7]
+
+data = list(enumerate(ids))
+print(data)
 # print(numbers)                  # [1, 2, 3, 4, 5]
 # ran = range(1,  6)
 # print(type(ran))
